de896e9a8ae1dbe62f5de44b5fef19bc25a17342
Dhanushbh <dd7254924@gmail.com>
2026-02-05 10:22:26 +0530

    Delete src/day4 directory

--- src/day4/inser.py
+++
@@ -1,8 +0,0 @@
-friend_a = {"Python", "Cooking", "Hiking", "Movies"}
-friend_b = {"Hiking", "Gaming", "Photography", "Python"}
-
-common_interests = friend_a & friend_b
-all_interests = friend_a | friend_b
-
-print("Common Interests:", common_interests)
-print("All Interests:", all_interests)
--- src/day4/keyvalue.py
+++
@@ -1,7 +0,0 @@
-person = {}
-
-person["name"] = "Dhanush"
-person["age"] = 22
-person["city"] = "Mangaluru"
-
-print(person)
