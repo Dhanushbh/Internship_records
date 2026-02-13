9fab01399096b561e79feab461041192c11cf2e9
Dhanushbh <dhanushbh@gmail.com>
2026-02-13 12:08:29 +0530

    day11

---
+++ src/day11/bar.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+names = ["A", "B", "C"]
+marks = [70, 85, 60]
+
+plt.bar(names, marks)
+plt.title("Bar Graph Example")
+plt.xlabel("Students")
+plt.ylabel("Marks")
+plt.show()
